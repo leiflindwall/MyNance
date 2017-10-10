ee44c154255fcf51737ce8c8a58b8ff424c1b6ee
kmholder <32622696+kmholder@users.noreply.github.com>
2017-10-09 17:07:53 -0700

    Updated Recurring page
    Recurring will now go back to home page once the expense is submitted. Keyboard now disappears when finished. Keyboard auto-capitalizes the beginning of each word.

---
+++ RecurringActivity.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using LoginSystem;
+using MyNance;
+
+namespace AndroidApp
+{
+    [Activity(Label = "Recurring Expenses/Deposits")]
+    public class RecurringActivity : Activity
+    {
+        string expense_Name = "";
+        string expense_Cost = "";
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Recurring);
+
+            EditText ExpenseCost = FindViewById<EditText>(Resource.Id.expenseCost);
+            ExpenseCost.TextChanged += (object sender, Android.Text.TextChangedEventArgs e) => {
+
+                expense_Cost = e.Text.ToString();
+            };
+
+            EditText ExpenseName = FindViewById<EditText>(Resource.Id.expenseName);
+            ExpenseName.TextChanged += (object sender, Android.Text.TextChangedEventArgs e) => {
+
+                expense_Name = e.Text.ToString();
+            };
+           Button Submit_Expense = FindViewById<Button>(Resource.Id.submitExpense);
+
+            Submit_Expense.Click += delegate
+            {
+                if (expense_Cost != "" && expense_Name != "")
+                {
+                    StartActivity(typeof(HomeActivity));
+                }
+            };
+
+
+        }
+
+       
+    }
+
+}
