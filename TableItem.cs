1ff8fbad7a1e61bce596c71a7ad26f11077ba0b9
leiflindwall <mikelindwall@gmail.com>
2017-11-04 16:39:49 -0700

    Transaction History Page
    add the three .cs files to the project, 2 .axml files to the /resources, and uncomment the start call in HomeActivity

---
+++ TableItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace MyNance_v1
+{
+    public class TableItem
+    {
+        public string Heading { get; set; }
+        public string SubHeading { get; set; }
+    }
+}
